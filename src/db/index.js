0a2cec9afcfe22ead51473bc274d11e6f0b42ac4
Dependra <dependrasingh027@gmail.com>
2025-07-11 17:23:01 +0530

    updated

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose, { connect } from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB= async()=>{
+  try {
+    //we can store the connection as well in some variables
+   const ConnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+   console.log(`\n mongoDB connected !! DB HOST: ${ConnectionInstance.connection.host}`);
+  }
+  catch(error)
+  {
+    console.error("Error", error);
+    //can also use exit method given offered process
+    process.exit(1);
+  }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+// Load environment variables
+dotenv.config();
+
+// import express from "express";
+
+// const app=express()
+
+// //making connection
+//   ( async()=>{
+   
+//     //use try catch every time u use db
+//     try {
+//     //   connection db
+//       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//       //using some feature of express
+//       app.on("error",(error)=>{
+//         console.log(error);
+//         throw error
+//       })
+//       //we can use listen as well
+//       app.listen(proces.env.PORT,()=>{
+//         console.log("server is running on port 3000");
+//       })
+
+//     }
+//     catch(error){
+//       console.error("Eoor", error)
+//       throw error
+//     }
+//   })()
+
+//* 2nd method- just import the db file and connect
+
+connectDB();
+
+
+
+
+
